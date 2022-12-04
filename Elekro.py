91b46b45961b3db034da972ca89c1e868236c377
Wundox <wundox@gmail.com>
2022-12-04 17:00:33 +0100

    v2

--- Elekro.py
+++ Elekro.py
@@ -5,10 +5,12 @@ import pyautogui
 from PIL import ImageGrab
 import numpy as np
 
-
-
-def isgrün(game_coords):
-    screenshot = ImageGrab.grab(bbox=game_coords,include_layered_windows=True,all_screens=True)
+# screenshot_fenster=[x1, y1, x2, y2]
+# farben=[r,g,b]
+# bsp. if hatFarben([780, 347, 1142, 763],[200, 200,0]):
+#           stop = False
+def hatFarbe(screenshot_fenster,farbe):
+    screenshot = ImageGrab.grab(bbox=screenshot_fenster,include_layered_windows=True,all_screens=True)
     erkennung = np.array(screenshot)
     #print(str(screenshot))
     xlen = len(erkennung)
@@ -24,13 +26,67 @@ def isgrün(game_coords):
     r = erkennung[midX,midY][0]
     g = erkennung[midX,midY][1]
     b = erkennung[midX,midY][2]
-    if [100] <= [g]:
+    
+    fr = farbe[0]
+    fg = farbe[1]
+    fb = farbe[2]
+
+    if fr <= r & fg <= g & fb <= b:
         print("‐●                                     ●‐")
         print("‐● Schalttafel wurde erfolgeich gelöst ●‐")
         print("‐●                                     ●‐")
         return True
     return False
 
+
+def beendeScript():
+    return hatFarbe([1860, 20, 1900, 70],[0,100,0])
+
+def istGruen():
+    return hatFarbe([1860, 20, 1900, 70],[0,100,0])
+
+
+def hatErsterBlock():
+    sx = 10
+    sy = 10
+    x1 = 1004
+    x2 = x1+sx
+    y1 = 580
+    y2 = y1+sy
+    ersterBlock = [x1, y1, x2, y2]
+    gelb=[200, 200,0] 
+    return hatFarbe(ersterBlock,gelb)
+
+def hatZweiterBlock():
+    sx = 10
+    sy = 10
+    x1 = 1050
+    x2 = x1+sx
+    y1 = 580
+    y2 = y1+sy
+    zweiterBlock = [x1, y1, x2, y2]
+    orange=[200, 200,0]
+    return hatFarbe(zweiterBlock,orange)
+
+def hatDritterBlock():
+    sx = 10
+    sy = 10
+    x1 = 1091
+    x2 = x1+sx
+    y1 = 580
+    y2 = y1+sy
+    dritterBlock = [x1, y1, x2, y2]
+    gruen=[100,60,0]
+    hatFarbe(dritterBlock,gruen)
+
+def BruteForceKabelLoesung():
+    # 3!=3*2*1=6
+    for i in range(6):
+        if not (hatErsterBlock() & hatZweiterBlock() & hatDritterBlock()):
+            print("alle blöcke verteilt! sind verbunden")
+            break
+        # TODO nicer algorithmus    
+    
 def anfang(game_coords):
     screenshot = ImageGrab.grab(bbox=game_coords,include_layered_windows=True,all_screens=True)
     erkennung = np.array(screenshot)
@@ -96,236 +152,77 @@ while True:
     while stop == False:
         print("‐● Im Schalttafelmodus ●‐")
       # Erster Durchgang
-        pyautogui .moveTo (1011 ,586 )
-        if isgrün([1860, 20, 1900, 70]):
-            stop = True
-            break
-        if stop == True:
-           break
-        pyautogui.dragTo(872, 594, button='left', duration=duration) #Gelb / Blau
-        if isgrün([1860, 20, 1900, 70]):
-            stop = True
-            break
-        if stop == True:
-           break
-        warten()
-        if isgrün([1860, 20, 1900, 70]):
-            stop = True
-            break
-        if stop == True:
-           break
-        pyautogui .mouseUp (button ='left')
-        if isgrün([1860, 20, 1900, 70]):
-            stop = True  
-        if stop == True:
-           break    
-        warten()
-        if isgrün([1860, 20, 1900, 70]):
-            stop = True
-        if stop == True:
-           break
-        pyautogui.moveTo(1053 , 591)
-        if isgrün([1860, 20, 1900, 70]):
-            stop = True
-        if stop == True:
-           break  
-        pyautogui.dragTo(874, 537, button='left', duration=duration)  # Orange / Pink
-        if isgrün([1860, 20, 1900, 70]):
-            stop = True
-        if stop == True:
-           break
-        warten()
-        if isgrün([1860, 20, 1900, 70]):
-            stop = True
-        if stop == True:
-           break  
-        pyautogui.mouseUp(button='left')
-        if isgrün([1860, 20, 1900, 70]):
-            stop = True
-        if stop == True:
-           break   
-        warten()
-        if isgrün([1860, 20, 1900, 70]):
-            stop = True
-        if stop == True:
-           break     
-        pyautogui.moveTo(1093, 592)
-        if isgrün([1860, 20, 1900, 70]):
-            stop = True
-        if stop == True:
-           break 
-        #print("‐● Im Schalttafelmodus ●‐")
-        pyautogui.dragTo(1055, 533, button='left', duration=duration)  # Grün / Braun
-        if isgrün([1860, 20, 1900, 70]):
-            stop = True
-        if stop == True:
-           break
-        warten()
-        if isgrün([1860, 20, 1900, 70]):
-            stop = True
-        if stop == True:
-           break  
-        pyautogui.mouseUp(button='left')
-        if isgrün([1860, 20, 1900, 70]):
-            stop = True  
-        if stop == True:
-           break    
-      
-             # Zweiter durchgang
-        time.sleep (0.1 )
-        if isgrün([1860, 20, 1900, 70]):
-            stop = True
-        if stop == True:
-           break    
-        pyautogui.moveTo(1011, 586)
-        if isgrün([1860, 20, 1900, 70]):
-            stop = True
-        if stop == True:
-           break  
-        print("‐● Im Schalttafelmodus ●‐")
-        pyautogui.dragTo(874, 537, button='left', duration=duration)
-        if isgrün([1860, 20, 1900, 70]):
-            stop = True  # Gelb / Pink
-        if stop == True:
-           break
-        warten()
-        if isgrün([1860, 20, 1900, 70]):
-            stop = True
-        if stop == True:
-           break  
-        pyautogui.mouseUp(button='left')
-        if isgrün([1860, 20, 1900, 70]):
-            stop = True  
-        if stop == True:
-           break       
-        warten()
-        if isgrün([1860, 20, 1900, 70]):
-            stop = True
-        if stop == True:
-           break    
-        pyautogui.moveTo(1053, 591)
-        if isgrün([1860, 20, 1900, 70]):
-            stop = True
-        if stop == True:
-           break 
-        #print("‐● Im Schalttafelmodus ●‐") 
-        pyautogui.dragTo(1055, 533, button='left', duration=duration)  # Orange / Braun
-        if isgrün([1860, 20, 1900, 70]):
-            stop = True
-        if stop == True:
-           break
-        warten()
-        if isgrün([1860, 20, 1900, 70]):
-            stop = True
-        if stop == True:
-           break  
-        pyautogui.mouseUp(button='left')
-        if isgrün([1860, 20, 1900, 70]):
-            stop = True
-        if stop == True:
-           break        
-        warten()
-        if isgrün([1860, 20, 1900, 70]):
-            stop = True
-        if stop == True:
-           break   
-        pyautogui.moveTo(1093, 592)
-        if isgrün([1860, 20, 1900, 70]):
-            stop = True
-        if stop == True:
-           break 
-        print("‐● Im Schalttafelmodus ●‐")
-        pyautogui.dragTo(872, 597, button='left', duration=duration)  # Grün / Blau
-        if isgrün([1860, 20, 1900, 70]):
-            stop = True
-        if stop == True:
-           break
-        warten()
-        if isgrün([1860, 20, 1900, 70]):
-            stop = True
-        if stop == True:
-           break  
-        pyautogui.mouseUp(button='left')
-        if isgrün([1860, 20, 1900, 70]):
-            stop = True
+        if hatErsterBlock():
+            pyautogui.moveTo (1011 ,586 )
+            pyautogui.dragTo(872, 594, button='left', duration=duration) #Gelb / Blau
+            pyautogui .mouseUp (button ='left')
+
+        if beendeScript() or stop == True:
+            break   
+
+        if hatZweiterBlock():
+            pyautogui.moveTo(1053 , 591)
+            pyautogui.dragTo(874, 537, button='left', duration=duration)  # Orange / Pink
+            pyautogui.mouseUp(button='left')
+
+        if beendeScript() or stop == True:
+            break 
+
+        if hatDritterBlock():
+            pyautogui.moveTo(1093, 592)
+            pyautogui.dragTo(1055, 533, button='left', duration=duration)  # Grün / Braun
+            pyautogui.mouseUp(button='left')
+            
+        if beendeScript() or stop == True:
+            break 
+
+        if hatErsterBlock():
+            pyautogui.moveTo(1011, 586)
+            pyautogui.dragTo(874, 537, button='left', duration=duration)
+            pyautogui.mouseUp(button='left')
+            print("‐● Im Schalttafelmodus ●‐")
+
+        if beendeScript() or stop == True:
+            break    
+
+        if hatZweiterBlock():
+            pyautogui.moveTo(1053, 591)
+            pyautogui.dragTo(1055, 533, button='left', duration=duration)  # Orange / Braun
+            pyautogui.mouseUp(button='left')
+            
         
-         # Dritter durchgang
+        if beendeScript() or stop == True:
+            break 
+
+        if hatDritterBlock(): 
+            pyautogui.moveTo(1093, 592)
+            pyautogui.dragTo(872, 597, button='left', duration=duration)  # Grün / Blau
+            pyautogui.mouseUp(button='left')
+            print("‐● Im Schalttafelmodus ●‐")
+
+        if beendeScript() or stop == True:
+            break
+
+        if hatErsterBlock():
+            pyautogui.moveTo(1011, 586)
+            pyautogui.dragTo(1055, 533, button='left', duration=duration)  # Gelb / Braun
+            pyautogui.mouseUp(button='left') 
+
+        if beendeScript() or stop == True:
+            break 
+
+        if hatZweiterBlock():
+            pyautogui.moveTo(1053, 591)
+            pyautogui.dragTo(872, 597, button='left', duration=duration)  # Orange / Blau
+            pyautogui.mouseUp(button='left') 
+            print("‐● Im Schalttafelmodus ●‐")
+
+        if beendeScript() or stop == True:
+            break 
+
+        if hatDritterBlock():
+            pyautogui.moveTo(1093, 592)
+            pyautogui.dragTo(874, 537, button='left', duration=duration)  # Grün / Pink
+            pyautogui.mouseUp(button='left') 
+            
         
-        if stop == True:
-           break    
-        warten()
-        if isgrün([1860, 20, 1900, 70]):
-            stop = True
-        if stop == True:
-           break   
-        pyautogui.moveTo(1011, 586)
-        if isgrün([1860, 20, 1900, 70]):
-            stop = True
-        if stop == True:
-           break 
-        #print("‐● Im Schalttafelmodus ●‐") 
-        pyautogui.dragTo(1055, 533, button='left', duration=duration)  # Gelb / Braun
-        if isgrün([1860, 20, 1900, 70]):
-            stop = True
-        if stop == True:
-           break
-        warten()
-        if isgrün([1860, 20, 1900, 70]):
-            stop = True
-        if stop == True:
-           break  
-        pyautogui.mouseUp(button='left') 
-        if isgrün([1860, 20, 1900, 70]):
-            stop = True  
-        if stop == True:
-           break      
-        warten()
-        if isgrün([1860, 20, 1900, 70]):
-            stop = True
-        if stop == True:
-           break    
-        pyautogui.moveTo(1053, 591)
-        if isgrün([1860, 20, 1900, 70]):
-            stop = True
-        if stop == True:
-           break  
-        print("‐● Im Schalttafelmodus ●‐")
-        pyautogui.dragTo(872, 597, button='left', duration=duration)  # Orange / Blau
-        if isgrün([1860, 20, 1900, 70]):
-            stop = True
-        if stop == True:
-           break
-        warten()
-        if isgrün([1860, 20, 1900, 70]):
-            stop = True
-        if stop == True:
-           break  
-        pyautogui.mouseUp(button='left') 
-        if isgrün([1860, 20, 1900, 70]):
-            stop = True 
-        if stop == True:
-           break    
-        warten()
-        if isgrün([1860, 20, 1900, 70]):
-            stop = True
-        if stop == True:
-           break    
-        pyautogui.moveTo(1093, 592)
-        if isgrün([1860, 20, 1900, 70]):
-            stop = True
-        if stop == True:
-           break 
-        #print("‐● Im Schalttafelmodus ●‐")
-        pyautogui.dragTo(874, 537, button='left', duration=duration)  # Grün / Pink
-        if isgrün([1860, 20, 1900, 70]):
-            stop = True
-        if stop == True:
-           break
-        warten()
-        if isgrün([1860, 20, 1900, 70]):
-            stop = True
-        if stop == True:
-           break  
-        pyautogui.mouseUp(button='left') 
-        if isgrün([1860, 20, 1900, 70]):
-            stop = True 
--- README.md
+++ README.md
@@ -1,3 +1,19 @@
-# info
+# Installiere Imports mit Befehl
 
-'pip install -r "recommentations.txt"'
+`pip install -r "recommentations.txt"`
+
+
+
+## schiebe Änderungen nach github
+
+1. öffne git bash im repo.
+2. mache `git add .`
+3. commit mit `git commit -m"das ist ein commit"`
+4. push mit `git push`
+
+
+## hole Änderungen von github
+
+1. öffne git bash im repo.
+2. mache `git fetch`
+3. ziehe neueste änderungen mit `git pull`
