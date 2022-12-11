aa636a6365a38365ae71fdf19238089550408ae4
Wundox <wundox@gmail.com>
2022-12-11 03:05:04 +0100

    V3

--- Olpumpen20.py
+++ Olpumpen20.py
@@ -1,28 +1,31 @@
-import mouse
-import keyboard
 import time
-from PIL import ImageGrab
+
+import keyboard
+import mouse
 import numpy as np
+from PIL import ImageGrab
 
 #game_coords = [653, 347, 1142, 763]
 
+
 def isWeiss(game_coords):
-    screenshot = ImageGrab.grab(bbox=game_coords,include_layered_windows=True,all_screens=True)
+    screenshot = ImageGrab.grab(
+        bbox=game_coords, include_layered_windows=True, all_screens=True)
     erkennung = np.array(screenshot)
     print(str(screenshot))
     xlen = len(erkennung)
     ylen = len(erkennung[0])
 
     print("x:" + str(xlen) + " y:" + str(ylen))
-    midX = xlen //2
-    midY = ylen //2
+    midX = xlen // 2
+    midY = ylen // 2
 
     print("mitte: x:" + str(midX) + " y:" + str(midY))
 
-    r = erkennung[midX,midY][0]
-    g = erkennung[midX,midY][1]
-    b = erkennung[midX,midY][2]
-    if [250, 250, 250] == [r,g,b]:
+    r = erkennung[midX, midY][0]
+    g = erkennung[midX, midY][1]
+    b = erkennung[midX, midY][2]
+    if [250, 250, 250] == [r, g, b]:
         print("is weiß")
         return True
     return False
@@ -42,23 +45,29 @@ def Anfang():
     ersterBlock = [x1, y1, x2, y2]
     gelb = [250, 200, 2]
     print("Start")
-    
+
+
+def drücken():
+    mouse.press('left')
+    time.sleep(2.6)
+    mouse.release('left')
+
 
 global stop
 stop = True
 
-def halten():
-   time.sleep (2.6)
 
 def start_event():
     global stop
     stop = False
     print("Start")
 
+
 def stop_event():
     global stop
     stop = True
     print("Wird Pausiert")
+
 
 keyboard.add_hotkey('q', lambda: start_event())
 keyboard.add_hotkey('e', lambda: stop_event())
@@ -69,62 +78,34 @@ while True:
     print("Zum Starten q drücken und e zum Pausieren")
     while stop == True:
         time.sleep(1)
-        if Anfang:
-            stop = False
-            
-        
-        
-        
-    # starte aufgabe 
+
     while stop == False:
-        x = 300
-        y = 600
-        mouse.move(x, y, absolute=True)
-        print("Maus wird gedrückt")
-        mouse.press('left')
-        print("Hält 2.4 sekunden")
-        halten()
-        mouse.release('left')
-        
+        mouse.move(300, 600, absolute=True)
+        drücken()
+
         if isWeiss([653, 347, 1142, 763]):
             stop = True
-        
+
         while stop == True:
             time.sleep(1)
             print("Warte")
-        
-        x2 = 400
-        mouse.move(x2, y, absolute=True)
-        print("Maus wird gedrückt")
-        mouse.press('left')
-        print("Hält 2.4 sekunden")
-        halten()
-        mouse.release('left')
-        
+
+        mouse.move(400, 600, absolute=True)
+        drücken()
+
         while stop == True:
             time.sleep(1)
             print("Warte")
-        
-        x3 = 500
-        mouse.move(x3, y, absolute=True)
-        print("Maus wird gedrückt")
-        mouse.press('left')
-        print("Hält 2.4 sekunden")
-        halten()
-        mouse.release('left')
-        
+
+        mouse.move(500, 600, absolute=True)
+        drücken()
+
         while stop == True:
             time.sleep(1)
             print("Warte")
-        
-        
-        x4 = 600
-        mouse.move(x4, y, absolute=True)
-        print("Maus wird gedrückt")
-        mouse.press('left')
-        print("Hält 2.5 sekunden")
-        halten()
-        mouse.release('left')
+
+        mouse.move(500, 600, absolute=True)
+        drücken()
 
         if isWeiss([653, 347, 1142, 763]):
             stop = True
