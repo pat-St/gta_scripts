e01a06935c68524af37e59146fab3bc1368a2a5d
Wundox <wundox@gmail.com>
2022-12-23 22:07:06 +0100

    V1

--- Benutzen.py
+++ Benutzen.py
@@ -1,24 +1,29 @@
-import mouse
-import keyboard
 import time
-import pyautogui
 
+import keyboard
+import mouse
+import pyautogui
 
 global stop
 stop = True
+counterLoop = 0
+
 
 def warten():
-   time.sleep (10)
+    time.sleep(6)
+
 
 def start_event():
     global stop
     stop = False
     print("Start")
 
+
 def stop_event():
     global stop
     stop = True
     print("Wird Pausiert")
+
 
 keyboard.add_hotkey('x', lambda: start_event())
 keyboard.add_hotkey('e', lambda: stop_event())
@@ -31,10 +36,18 @@ while True:
     while stop == True:
         time.sleep(2)
 
-    
-        
     while stop == False:
         keyboard.press('2')
         time.sleep(0.2)
         keyboard.release('2')
         warten()
+        counterLoop += 1
+
+        if counterLoop > 20:
+            keyboard.press('w')
+            time.sleep(1)
+            keyboard.release('w')
+            keyboard.press('s')
+            time.sleep(1)
+            keyboard.release('s')
+            counterLoop = 0
