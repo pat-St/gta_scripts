967ea67689c62a6b9482621798a5f9c92e449daf
Wundox <wundox@gmail.com>
2024-04-01 00:47:34 +0200

    fertig

--- repkitkaufen.py
+++ repkitkaufen.py
@@ -21,81 +21,42 @@ def tankeöffnen():
 
 def repkitkaufen():
     tankeöffnen()
-    mouse.move(1016, 602, absolute=True)
     warten()
-    counterLoop = 0
-    while counterLoop < 10:
-        print("klick")
-        mouse.click('left')
-        if stop == True:
-            break
-        time.sleep(0.53)
-        counterLoop += 1
-    print("fertig 10 stpck")
-    warten()
-    mouse.move(1270, 450, absolute=True)
-    warten()
-    mouse.click('left')
-    warten()
+    for i in range(int(choice_repkit)):
+        mouse.move(1016, 602, absolute=True)
+        warten()
+        counterLoop = 0
+        while counterLoop < 10:
+            mouse.click('left')
+            if stop == True:
+                break
+            time.sleep(0.6)
+            counterLoop += 1
+        print("10 stück wurden gekauft")
+        warten()
+        kaufbestätigen()
 
 
 def kanisterkaufen():
     tankeöffnen()
-    mouse.move(1016, 602, absolute=True)
     warten()
-    counterLoop = 0
-    for i in range(choice_kanister):
+    for i in range(int(choice_kanister)):
+        mouse.move(1016, 725, absolute=True)
         print("klick")
         mouse.click('left')
         if stop == True:
             break
-        time.sleep(0.53)
-        counterLoop += 1
-    print("fertig 10 stpck")
+        time.sleep(0.6)
+    print(f"{choice_kanister} stück Kanister wurden gekauft")
     warten()
+    kaufbestätigen()
+
+
+def kaufbestätigen():
     mouse.move(1270, 450, absolute=True)
     warten()
     mouse.click('left')
     warten()
-
-
-def kanisterdurchgänge():
-    kanisterkaufen()
-    warten()
-    inventar()
-
-
-def repkitdurchgänge():
-    if choice == '1':
-        print("loop 1")
-        repkitkaufen()
-        warten()
-        inventar()
-    elif choice == '2':
-        print("loop 2")
-        for i in range(2):
-            print("test2")
-            repkitkaufen()
-        warten()
-        inventar()
-    elif choice == '3':
-        print("loop 3")
-        for i in range(3):
-            print("test3")
-            repkitkaufen()
-        warten()
-        inventar()
-    elif choice == '4':
-        print("test4")
-        print("loop 4")
-        for i in range(4):
-            print("test4")
-            repkitkaufen()
-        warten()
-        inventar()
-    else:
-        print("Ungültige Auswahl. Bitte wähle eine Option von 1 bis 4.")
-        quit()
 
 
 def inventar():
@@ -136,44 +97,48 @@ keyboard.add_hotkey('x', lambda: start_event())
 keyboard.add_hotkey('e', lambda: stop_event())
 
 print("")
-selection = input("Kanister (y) oder Repkit (n):")
+selection = input("Kanister (1) oder Repkit (2):")
 print("")
+
+print("---------------------")
+print("Empfohlen von 0.30 bis 99 sekunden")
+wartezeit = float(
+    input("Gib die Wartezeit zwischen den Eingaben in Sekunden ein: "))
+
+print("---------------------")
 
 IS_REPKIT = False
 
-if (len(selection) > 0) and (str(selection).lower() == 'y'):
+if (len(selection) > 0) and (str(selection).lower() == '1'):
     IS_REPKIT = True
-    print("---------------------")
-    print("Kanister Aktiviert")
+    # print("---------------------")
+    # print("Kanister Aktiviert")
+    # print("---------------------")
 
 
 else:
     IS_REPKIT = False
-    print("---------------------")
-    print("Repkit Aktiviert")
+    # print("---------------------")
+    # print("Repkit Aktiviert")
+    # print("---------------------")
 
 
-print("---------------------")
-wartezeit = int(
-    input("Gib die Wartezeit zwischen den Eingaben in Sekunden ein: "))
-print("---------------------")
 if IS_REPKIT == False:
     print("------------Repkits Ausgewählt------------")
     print(' 1 ausführung = 10 repkits:')
-    print(' wähle zwischen 1 - 4 durchgänge ')
+    print(' wähle zwischen 1 - 99 durchgänge ')
     print(" 1st Slot Inventar und Auto")
     print("")
-    choice = input("Deine Auswahl: ")
+    choice_repkit = input("Deine Auswahl: ")
     print("---------------------")
 else:
     print("------------Kanister Ausgewählt------------")
-    print(' 1 ausführung = 3 Kanister:')
-    print(' wähle zwischen 1 - 4 durchgänge ')
+    print(' wie viele Kanister willst du in einem Durchgang kaufen:')
+    print(' wähle zwischen 1 - 99 ')
     print(" 1st Slot Inventar und Auto")
     print("")
     choice_kanister = input("Deine Auswahl: ")
     print("---------------------")
-
 
 while True:
 
@@ -188,8 +153,14 @@ while True:
             break
 
         if IS_REPKIT == False:
-            repkitdurchgänge()
+            print("Repkits")
+            repkitkaufen()
+            warten()
+            inventar()
         else:
-            kanisterdurchgänge()
+            print("Kanister")
+            kanisterkaufen()
+            warten()
+            inventar()
         if stop == True:
             break
